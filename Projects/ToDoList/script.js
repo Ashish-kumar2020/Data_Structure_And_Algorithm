a45e038601f394cc1a588982a022f4102f730636
Ashish Kumar Singh <69068290+Ashish-kumar2020@users.noreply.github.com>
2024-03-12 08:53:47 +0530

    add new todo functionality

--- Projects/ToDoList/script.js
+++ Projects/ToDoList/script.js
@@ -4,3 +4,36 @@ function goToLoginPage() {
   console.log("Login page");
   window.open("Login.html");
 }
+
+let todoinputBox = document.querySelector("#todoinputBox");
+let submitBtn = document.querySelector("#submitBtn");
+let container = document.querySelector("#container");
+
+function addTodo() {
+  let inputValue = todoinputBox.value;
+  if (inputValue.length === 0) {
+    alert("Todo cannot be empty");
+  } else {
+    let unorderedTask = document.createElement("ul");
+    unorderedTask.classList = "list-group";
+
+    let li = document.createElement("li");
+    li.classList = "list-group-item";
+
+    li.textContent = inputValue;
+
+    unorderedTask.append(li);
+    container.appendChild(unorderedTask);
+  }
+
+  console.log(inputValue);
+  todoinputBox.value = "";
+}
+
+//  <ul class="list-group">
+//     <li class="list-group-item">An item</li>
+//     <li class="list-group-item">A second item</li>
+//     <li class="list-group-item">A third item</li>
+//     <li class="list-group-item">A fourth item</li>
+//     <li class="list-group-item">And a fifth one</li>
+//   </ul>;
