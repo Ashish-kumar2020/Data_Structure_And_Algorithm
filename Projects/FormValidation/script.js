f18b5a66639ed41fe3176c1f63219679edda065a
Ashish Kumar Singh <69068290+Ashish-kumar2020@users.noreply.github.com>
2024-03-14 09:17:08 +0530

    form validation(login signup)

--- Projects/FormValidation/script.js
+++ Projects/FormValidation/script.js
@@ -1,11 +1,18 @@
 console.log("Form Validation");
-
 let userName = document.querySelector("#userName");
 let userEmail = document.querySelector("#userEmail");
 let userPassword = document.querySelector("#userPassword");
 let confirmPassword = document.querySelector("#confirmPassword");
 let phoneNumber = document.querySelector("#phoneNumber");
 let count = 1;
+
+let details = {
+  name: "Ashish Singh",
+  email: "ashishsinghk2020@gmail.com",
+  password: "12345",
+  phoneNumber: "07302433809",
+};
+details.confirmPassword = details.password;
 function registerDetails() {
   if (
     userName.value.length === 0 ||
@@ -25,7 +32,24 @@ function registerDetails() {
       phoneNumber: phoneNumber.value,
     };
     console.log(user);
-    localStorage.setItem(`user${count}`, JSON.stringify(user));
+    localStorage.setItem("userData", JSON.stringify(user));
     count++;
+
+    validateData(user);
+  }
+}
+function validateData(userDetails) {
+  if (
+    userDetails.userName === details.name &&
+    userDetails.userEmail === details.email &&
+    userDetails.userPassword === details.password &&
+    userDetails.confirmPassword === details.password &&
+    userDetails.phoneNumber === details.phoneNumber
+  ) {
+    console.log("User Login");
+    window.location.href = "login.html";
+  } else {
+    console.log("User details do not match.");
+    window.location.href = "register.html";
   }
 }
