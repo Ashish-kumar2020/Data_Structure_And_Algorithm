1277497e24188fefed51cb6e9854266511a29a2f
Ashish Kumar Singh <69068290+Ashish-kumar2020@users.noreply.github.com>
2024-03-21 23:47:31 +0530

    added api data

--- Projects/FoodDelivery/script/carausal.js
+++ Projects/FoodDelivery/script/carausal.js
@@ -1,3 +1,15 @@
 import { API_DATA } from "./API.js";
 
-console.log(API_DATA);
+const restaurant = document.querySelector("#restaurants");
+
+console.log(restaurant);
+
+let data = API_DATA.restaurants;
+
+const innerArray = API_DATA.restaurants[0];
+
+// const singleElementArray = innerArray;
+// const element = singleElementArray[0];
+
+console.log(innerArray);
+// console.log(COMMON_URL);
