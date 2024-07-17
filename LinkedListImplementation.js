0b6705194af1aae7698e25e923e52c202ea10038
Ashish Kumar Singh <69068290+Ashish-kumar2020@users.noreply.github.com>
2024-07-18 00:03:09 +0530

    Learn about insertion and deletion in linked list

--- LinkedListImplementation.js
+++ LinkedListImplementation.js
@@ -49,6 +49,113 @@ class LinkedList {
     }
     return 0;
   }
+
+  // Delete Head of Linked List
+  deleteHead() {
+    if (this.head === null) return this.head;
+    let temp = this.head;
+    this.head = this.head.next;
+    return this.head;
+  }
+
+  // Delete Tail of Linked List
+  deleteTail() {
+    if (this.head === null || this.head.next === null) return null;
+    let temp = this.head;
+    while (temp.next.next !== null) {
+      temp = temp.next;
+    }
+    temp.next = null;
+    return this.head;
+  }
+
+  // Delete At Kth Position
+  deleteAtKPosition(k) {
+    if (this.head === null) return this.head;
+    if (k == 1) {
+      this.head = this.head.next;
+      return this.head;
+    }
+    let prev = null;
+    let temp = this.head;
+    let cnt = 0;
+    while (temp !== null) {
+      cnt++;
+      if (cnt == k) {
+        prev.next = temp.next;
+        break;
+      }
+      prev = temp;
+      temp = temp.next;
+    }
+    return this.head;
+  }
+
+  // Delete Elemenet
+  deleteElement(ele) {
+    if (this.head === null) return this.head;
+    if (this.head.value == ele) {
+      this.head = this.head.next;
+      return this.head;
+    }
+    let prev = null;
+    let temp = this.head;
+
+    while (temp !== null) {
+      if (temp.value === ele) {
+        prev.next = temp.next;
+        break;
+      }
+      prev = temp;
+      temp = temp.next;
+    }
+    return this.head;
+  }
+
+  // Insert At Head
+  insertAtHead(val) {
+    let temp = new Node(val);
+    temp.next = this.head;
+    this.head = temp;
+    return this.head;
+  }
+
+  // Insert At Tail
+  insertAtTail(val) {
+    let temp = new Node(val);
+    let curr = this.head;
+    while (curr.next.next !== null) {
+      curr = curr.next;
+    }
+    curr.next.next = temp;
+    temp.next = null;
+    return this.head;
+  }
+
+  // Insert At Kth Position
+  insertAtKPosition(k, val) {
+    if (this.head === null) return null;
+    if (k == 1) {
+      let temp = new Node(val);
+      temp.next = this.head;
+      this.head = temp;
+      return this.head;
+    }
+    let curr = this.head;
+    let cnt = 0;
+    while (curr !== null) {
+      cnt++;
+      if (cnt == k - 1) {
+        let newNode = new Node(val);
+        newNode.next = curr.next;
+        curr.next = newNode;
+        return this.head;
+      }
+      curr = curr.next;
+    }
+    return this.head;
+  }
+
   //   Print Linked List
   printLinkedList() {
     let currNode = this.head;
@@ -61,7 +168,18 @@ class LinkedList {
   }
 }
 const list = new LinkedList();
-list.converArrayToLinkedList([1, 2, 3, 4, 5]);
-list.printLinkedList();
+list.converArrayToLinkedList([1, 2, 37, 4, 50]);
 console.log(list.printLinkedListLength());
 console.log(list.searchElement(1));
+// list.deleteHead();
+// list.deleteHead();
+// list.deleteTail();
+// list.deleteTail();
+// list.deleteAtKPosition(4);
+// list.deleteElement(37);
+// list.insertAtHead(10);
+// list.insertAtTail(345);
+// list.insertAtTail(3455);
+list.insertAtKPosition(2, 300);
+list.insertAtKPosition(3, 300);
+list.printLinkedList();
